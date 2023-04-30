6e6baea6848828b8a3121cc60c12777743062e37
KaoutharAkha <kaoukhouzan@example.com>
2023-04-30 12:39:09 -0700

    try

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -29,6 +29,8 @@ int main(void)
 						putchar(' ');
 						putchar('0' + k);
 						putchar('0' + l);
+						if (i == 9 && j == 8 && k == 9 && l == 9)
+							break;
 						putchar(',');
 						putchar(' ');
 					}
