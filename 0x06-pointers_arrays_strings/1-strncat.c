7d0c82f9e4131c80be654bf93348fdf05de48335
KaoutharAkha <kaoukhouzan@gmail.com>
2023-07-30 12:17:16 -0700

    Fixing a tap error in the function strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * _strncat - concatenates two strings.
- * @dest : input value
- * @src : input value
- * @n : input value
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  *
  * Return: dest
  */
@@ -15,13 +15,15 @@ char *_strncat(char *dest, char *src, int n)
 
 	i = 0;
 	while (dest[i] != '\0')
-		i++;
-	j = 0;
-	while (src[j] != '\n' && j < n)
 	{
-		dest[i] = src[j];
 		i++;
-		j++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
 	}
 	dest[i] = '\0';
 	return (dest);
